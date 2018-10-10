6601b6eda6e15612a5813ebb89cd7fad3b00ecd5
Igor Pejic <igor.pejic@dr.com>
2018-10-10 22:20:19 +0200

    plot MonteCarlo simulation step efficiency

--- KL.py
+++ KL.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt 
 
 def normpdf(x, mean, sd, steps):
     pi = 3.1415926
@@ -24,17 +25,28 @@ def kl_extracted_forumla_1(mu, sigma, ni, tau, steps):
     return 0.5 * (math.log((tau*tau)/(sigma*sigma)) -1 + (sigma*sigma + (mu - ni)**2) / (tau * tau))
 
 mu = 0.3
-sigma =  0.3
+sigma =  0.4
 
-steps = 400
-p = np.array([normpdf(x, mu, sigma, steps) for x in np.linspace(-100, 100, steps)])
-p = p / sum(p)
+monte_carlo_success_rate = np.array([])
+for i in range(1, 2000):
+    steps = i
+    p = np.array([normpdf(x, mu, sigma, steps) for x in np.linspace(-100, 100, steps)])
+    p = p / sum(p)
 
-ni = 0.1
-tau = 0.2
-q = np.array([normpdf(x, ni, tau, steps) for x in np.linspace(-100, 100, steps)])
-q = q / sum(q)
+    ni = 0.1
+    tau = 0.2
+    q = np.array([normpdf(x, ni, tau, steps) for x in np.linspace(-100, 100, steps)])
+    q = q / sum(q)
 
-print(kl(p, q))
-print(kl_extracted_forumla_1(mu, sigma, ni, tau, steps))
-print(kl_extracted_forumla(mu, sigma, ni, tau, steps))
+    mc_kl = kl(p, q)
+    kl_from_formula = (kl_extracted_forumla_1(mu, sigma, ni, tau, steps))
+    #print(kl_from_formula)
+    #print(mc_kl)
+
+    monte_carlo_success_rate = np.append(monte_carlo_success_rate, mc_kl/kl_from_formula)
+
+plt.plot(monte_carlo_success_rate)
+plt.title('Ratio of Monte-Carlo obtained KL value vs KL value obtained by definition')
+plt.xlabel('Monte-Carlo steps')
+plt.ylabel('Monte-Carlo value / formula value')
+plt.show()
